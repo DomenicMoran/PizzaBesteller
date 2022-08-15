4e7701276392f683194a43fe28c222340a7d432b
Domenic <domenicmoran@gmail.com>
2022-08-15 18:10:08 +0200

    erste Funktionen

--- ContentView.swift
+++ ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var pizza = Pizza()
+    @State private var selectedIndex = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Wähle deine Pizza aus", selection: $pizza.pizzaTypeIndex) {
+                        ForEach(0..<Pizza.pizzaTypes.count) { index in
+                            Text(Pizza.pizzaTypes[index]).tag(index)
+                        }
+                    }
+                    
+                    Stepper(value: $pizza.anzahl, in: 1...8) {
+                        Text("Anzahl an Pizzen: \(pizza.anzahl)")
+                    }
+                }
+                Section {
+                    Picker("", selection: $pizza.pizzaSizeIndex) {
+                        ForEach(0..<Pizza.pizzaSizes.count) {
+                            Text(Pizza.pizzaSizes[$0]).tag($0)
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Lieferung")) {
+                    
+                }
+            }
+            .navigationBarTitle("Pizza Bestellung")
+        }
     }
 }
 
---
+++ Pizza.swift
@@ -0,0 +1,20 @@
+//
+//  Pizza.swift
+//  PizzaBesteller
+//
+//  Created by Domenic Moran on 15.08.22.
+//
+
+import Foundation
+
+class Pizza: ObservableObject {
+    static let pizzaTypes = ["Salami", "Funghi", "Margherita", "Mozzarella", "Etna", "Tonno", "Ragu", "Peperoni"]
+    
+    static let pizzaSizes = ["Small", "Medium", "Large"]
+    
+    @Published var pizzaTypeIndex = 0
+    
+    @Published var anzahl = 1
+    
+    @Published var pizzaSizeIndex = 0
+}
