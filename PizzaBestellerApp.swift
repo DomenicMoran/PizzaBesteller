b22032490e1f8ffc77ce369a0b392aab6c716e5c
Domenic <domenicmoran@gmail.com>
2022-08-15 17:29:15 +0200

    Anfang

---
+++ PizzaBestellerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaBestellerApp.swift
+//  PizzaBesteller
+//
+//  Created by Domenic Moran on 15.08.22.
+//
+
+import SwiftUI
+
+@main
+struct PizzaBestellerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
